422f850b1ef8531533f962634802eeb2e67986a7
gennei <sai.gennei+github@gmail.com>
2022-11-05 15:34:54 +0900

    stage3 test passed

--- app/main.php
+++ app/main.php
@@ -1,15 +1,34 @@
 <?php
 error_reporting(E_ALL);
+const PORT = 6379;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 echo "Logs from your program will appear here" . PHP_EOL;
 
 $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_set_option($sock, SOL_SOCKET, SO_REUSEPORT, 1);
-socket_bind($sock, "localhost", 6379);
+socket_bind($sock, "localhost", PORT);
 socket_listen($sock, 5);
 $socket = socket_accept($sock);
-$response = "+PONG\r\n";
-socket_write($socket, $response, strlen($response));
 
+while (true) {
+    $message = socket_read($socket, 2048, PHP_NORMAL_READ);
+    $message = trim($message);
+    if ($message === "") {
+        continue;
+    }
+    if ($message === "exit") {
+        $res = "👋👋👋\r\n";
+        socket_write($socket, $res, strlen($res));
+        break;
+    }
+    if ($message === "ping") {
+        $response = "+PONG\r\n";
+        socket_write($socket, $response, strlen($response));
+        continue;
+    }
+
+    echo $message . " is not support command" . PHP_EOL;
+}
+socket_close($socket);
 socket_close($sock);
